28846aa84da3e91cee874b58b246b5b223bf206d
foxeltm <fuchs.raffael@hotmail.com>
2025-11-26 21:46:43 +0100

    erster commit

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main () 
+{
+	int a = 10;
+	int y = 20;
+	int sum = a + y;
+	std::string b = "Hello";
+	char c = 'A';
+
+	std::cout << b << " " << c << " \n";
+	std::cout << a << "\n";
+	std::cout << y << "\n";
+	std::cout << "Sum is: " << sum << "\n";
+
+
+
+
+	std::cin.get();
+}
