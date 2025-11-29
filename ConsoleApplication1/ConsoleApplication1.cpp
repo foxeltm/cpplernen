c22138a1fe8d158c1b427728d0439de849a3f62d
foxeltm <fuchs.raffael@hotmail.com>
2025-11-29 20:33:27 +0100

    Hypotenuse calculator

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -3,21 +3,19 @@
 
 int main () 
 {
-	double x = 3.99;
-	double y = 4;
-	double z;
+	double a;
+	double b;
+	double c;
 
-	//z = std::max(x, y);
-	//z = std::min(x, y);
-	//z = pow(2, 4);
-	//z = sqrt(9);
-	//z = abs(-5);
-	//z = round(4.6);
-	//z = ceil(4.2);
-	//z = floor(x);
+	std::cout << "enter side a ";
+	std::cin >> a;
 
+	std::cout << "enter side b ";
+	std::cin >> b;
 
-	std::cout << z;
+	c = std::sqrt(pow(a, 2) + pow(b, 2);
+
+	std::cout << "hypotenuse = " << c << std::endl;
 
 
 	std::cin.get();
