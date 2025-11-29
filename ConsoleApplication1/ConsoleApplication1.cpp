9c526dc3982c3de1196dac61ba48900f4c79d0ce
foxeltm <fuchs.raffael@hotmail.com>
2025-11-29 23:10:39 +0100

    switch

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -3,44 +3,34 @@
 
 int main () 
 {
-	bool playagain = true;
+	char grade;
 
-	while (playagain)
+	std::cout << "What letter grade?: ";
+	std::cin >> grade;
+
+	switch(grade) 
 	{
-		double a;
-		double b;
-		double c;
-
-		std::cout << "enter side a ";
-		std::cin >> a;
-
-		std::cout << "enter side b ";
-		std::cin >> b;
-
-		c = std::sqrt(pow(a, 2) + pow(b, 2));
-
-		std::cout << "hypotenuse = " << c << std::endl;
-
-		std::cout << "play again? (1 = yes, 0 = no) ";
-
-		int answer;
-
-		std::cin >> answer;
-		
-		if(answer == 0)
-		{
-			playagain = false;
-		}
-		else if (answer == 1)
-		{
-			playagain = true;
-		}
-		else
-		{
-			std::cout << "invalid input, exiting program." << std::endl;
-			playagain = false;
-		}
+		case 'A':
+			std::cout << "Excellent!" << std::endl;
+			break;
+		case 'B':
+			std::cout << "Well done" << std::endl;
+			break;
+		case 'C':	
+			std::cout << "Good" << std::endl;
+			break;
+		case 'D':
+			std::cout << "You passed" << std::endl;
+			break;
+		case 'F':
+			std::cout << "Better try again" << std::endl;
+			break;
+		default:
+			std::cout << "Invalid grade" << std::endl;
 
 	}
+	
+
+
 	std::cin.get();
 }
