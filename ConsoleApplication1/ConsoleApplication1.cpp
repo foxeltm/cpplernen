525491ba330c9b3e1373cb14f46ffe40e586df44
foxeltm <fuchs.raffael@hotmail.com>
2025-11-30 17:49:56 +0100

    Temperature conversion program 🌡️

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,17 +1,68 @@
 #include <iostream>
 
 int main()
-{
-	//int grade;
-	//(grade >= 60) ? std::cout << "Passed\n" : std::cout << "Failed\n";
+{	
 
-	//int number = 9;
-	//number % 2 ? std::cout << "ODD" : std::cout << "EVEN";
+	bool playagain = true;
 
-	bool hungry = true;
+	std::cout << "XXXXXXX Temperature conversion XXXXXXX\n";
 
-	//hungry ? std::cout << "Feed me!" : std::cout << "I'm not hungry";
-	std::cout << (hungry ? "Feed me!" : "I'm not hungry");
+	while (playagain)
+	{
+		double temp;
+		char unit;
+
+		std::cout << "F = Fahrenheit\n";
+		std::cout << "C = Celsius\n";
+		std::cout << "what unit would you like to convert to ";
+		std::cin >> unit;
+
+		if (unit == 'F' || unit == 'f')
+		{
+			std::cout << "Enter temperature in Celsius: ";
+			std::cin >> temp;
+
+			temp = (1.8 * temp) + 32;
+			std::cout << "Temperature in Fahrenheit: " << temp << " F\n";
+		}
+		else if (unit == 'C' || unit == 'c')
+		{
+			std::cout << "Enter temperature in Fahrenheit: ";
+			std::cin >> temp;
+
+			temp = (temp - 32) / 1.8;
+			std::cout << "Temperature in Celsius: " << temp << " C\n";
+		}
+		else
+		{
+			std::cout << "Invalid unit entered.\n please enter C or F\n";
+		}
+
+		std::cout << "Would you like to convert another temperature? (y/n): ";
+		
+		char response;
+		std::cin >> response;
+
+		if(response == 'n' || response == 'N')
+		{
+			playagain = false;
+		}
+		else if (response == 'y' || response == 'Y')
+		{
+			playagain = true;
+		}
+		else
+		{
+			std::cout << "Invalid response. Exiting the program.\n";
+			playagain = false;
+		}
+
+
+	}
+
+
+	
+
 
 	std::cin.get();
 }
