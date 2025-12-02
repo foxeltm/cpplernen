f92ea11da23891ec9d68b39ef3e6a2d835cdd1cb
foxeltm <fuchs.raffael@hotmail.com>
2025-12-02 23:30:58 +0100

    C++ create a random number generator 🎲

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -2,28 +2,16 @@
 
 int main()
 {	
-    int rows;
-    int columns;
-    char symbol;
+	srand(time(NULL));
 
-    std::cout << "How many rows?: ";
-    std::cin >> rows;
+	int num1 = (rand() % 20) + 1;
+	int num2 = (rand() % 20) + 1;
+	int num3 = (rand() % 20) + 1;
+		
 
-    std::cout << "How many columns?: ";
-    std::cin >> columns;
-
-    std::cout << "Enter a symbol to use: ";
-    std::cin >> symbol;
-
-    for (int i = 1; i <= rows; i++)
-    {
-        for (int j = 1; j <= columns; j++)
-        {
-            std::cout << symbol;
-        }
-        std::cout << '\n';
-    }
-
+	std::cout << "Random number: " << num1 << std::endl;
+	std::cout << "Random number: " << num2 << std::endl;
+	std::cout << "Random number: " << num3 << std::endl;
 
 
 	std::cin.get();
