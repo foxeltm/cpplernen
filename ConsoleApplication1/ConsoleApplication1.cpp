0a8382f4b80f7e548158f532cbb2614c29a6ff20
foxeltm <fuchs.raffael@hotmail.com>
2025-12-02 23:24:13 +0100

    nested loops

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -2,18 +2,27 @@
 
 int main()
 {	
-	// break = break out of a loop
-	// continue = skip current iteration
+    int rows;
+    int columns;
+    char symbol;
 
-	for (int i = 1; i <= 20; i++)
-	{
-		if (i == 13) {
-			//break;
-			continue;
-		}
-			
-		std::cout << i << std::endl;
-	}
+    std::cout << "How many rows?: ";
+    std::cin >> rows;
+
+    std::cout << "How many columns?: ";
+    std::cin >> columns;
+
+    std::cout << "Enter a symbol to use: ";
+    std::cin >> symbol;
+
+    for (int i = 1; i <= rows; i++)
+    {
+        for (int j = 1; j <= columns; j++)
+        {
+            std::cout << symbol;
+        }
+        std::cout << '\n';
+    }
 
 
 
