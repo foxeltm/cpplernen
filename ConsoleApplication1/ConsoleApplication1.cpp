505ae802378915bfa478be307cfcafa7d5753be1
foxeltm <fuchs.raffael@hotmail.com>
2025-12-03 13:26:16 +0100

    user defined functions

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,53 +1,24 @@
 #include <iostream>
 #include <ctime>
 
+void happyBirthday(std::string name, int age);
+
 int main()
 {	
-    std::cout << "Welcome to the Number Guessing Game!" << std::endl;
+	std::string name = "Raffael";
+	int age = 14;
 
-    bool playing = true;
 
-    srand(time(0)); 
-
-    while (playing)
-    {
-        int num = 0;
-        int guess = 0;
-        int tries = 0;
-
-        num = (rand() % 100) + 1;
-
-        do
-        {
-            std::cout << "Enter your guess (1-100): ";
-            std::cin >> guess;
-            tries++;
-
-            if (guess > num)
-            {
-                std::cout << "Too high! Try again." << std::endl;
-            }
-            else if (guess < num)
-            {
-                std::cout << "Too low! Try again." << std::endl;
-            }
-            else
-            {
-                std::cout << "Congratulations! You've guessed the number "
-                    << num << " in " << tries << " tries." << std::endl;
-            }
-
-        } while (guess != num);
-
-        std::cout << "Do you want to play again? (y/n): ";
-        char response;
-        std::cin >> response;
-
-        if (response != 'y' && response != 'Y')
-        {
-            playing = false;
-        }
-    }
+	happyBirthday(name, age);
+	
 
 	std::cin.get();
+}
+void happyBirthday(std::string name, int age)
+{
+	std::cout << "Happy Birthday to " << name << '\n';
+	std::cout << "Happy Birthday to " << name << '\n';
+	std::cout << "Happy Birthday Dear" << name << '\n';
+	std::cout << "Happy Birthday to " << name << '\n';
+	std::cout << "You are now " << age << " years old!" << '\n';
 }
