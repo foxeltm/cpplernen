70f7acdb804522ac51567e567b5bd27f38a4b3de
foxeltm <fuchs.raffael@hotmail.com>
2025-12-04 14:56:11 +0100

    verbesserung von return

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,20 +1,44 @@
 #include <iostream>
-#include <ctime>
+#include <string>
 
-std::string concatStrings(std::string sting1, std::string string2);
+std::string concatStrings(std::string string1, std::string string2);
 
 int main()
-{	
-	std::string firstname = "raffael";
-	std::string lastname = "fuchs";
-	std::string fullname = concatStrings(firstname, lastname);
-
-	std::cout << "Hello, " << fullname << "!" << std::endl;
-	
-
-	std::cin.get();
-}
-std::string concatStrings(std::string sting1, std::string string2) 
 {
-	return sting1 + " " + string2;
+    bool play = true;
+
+    while (play)
+    {
+        std::string firstname;
+        std::string lastname;
+
+        std::cout << "gib deinen vornamen ein: ";
+        std::cin >> firstname;
+
+        std::cout << "gib deinen nachnamen ein: ";
+        std::cin >> lastname;
+
+        std::string fullname = concatStrings(firstname, lastname);
+
+        std::cout << "Hello, " << fullname << "!" << std::endl;
+        std::cout << "Moechtest du es nochmal versuchen? (j/n): ";
+
+        char response;
+        std::cin >> response;  // besser!
+
+        if (response == 'n' || response == 'N')
+            play = false;
+        else if (response == 'j' || response == 'J')
+            play = true;
+        else
+        {
+            std::cout << "ungueltige eingabe, das programm wird beendet." << std::endl;
+            play = false;
+        }
+    }
+}
+
+std::string concatStrings(std::string string1, std::string string2)
+{
+    return string1 + " " + string2;
 }
