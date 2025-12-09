0538ca2c32bafc641b9f8763be1cdda3e3311a2d
foxeltm <fuchs.raffael@hotmail.com>
2025-12-09 18:26:51 +0100

    credit card

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -2,20 +2,59 @@
 #include <ctime>
 #include <string>
 
-void printInfo(const std::string &name, const int &age);
+int getDigit(const int number);
+int sumOddDigits(const std::string cardNumber);
+int sumEvenDigits(const std::string cardNumber);
 
 int main()
 {	
-    std::string name = "Raffel";
-    int age = 14;
+ 
+    std::string cardNumber;
+    int result = 0;
 
-    printInfo(name, age);
+    std::cout << "Enter your credit card number:";
+    std::cin >> cardNumber;
+
+    result = sumEvenDigits(cardNumber) + sumOddDigits(cardNumber);
+
+    if (result % 10 == 0)
+    {
+        std::cout << cardNumber << " is valid";
+    }
+    else
+    {
+        std::cout << cardNumber << " is not valid";
+    }
 
 
     std::cin.get();
 }
-void printInfo(const std::string &name,const int &age)
+int getDigit(const int number)
 {
-    std::cout << name << '\n';
-    std::cout << age << '\n';
+
+    return number % 10 + (number / 10 % 10);
+}
+int sumOddDigits(const std::string cardNumber)
+{
+
+    int sum = 0;
+
+    for (int i = cardNumber.size() - 1; i >= 0; i -= 2)
+    {
+        sum += cardNumber[i] - '0';
+    }
+
+    return sum;
+}
+int sumEvenDigits(const std::string cardNumber)
+{
+    int sum = 0;
+
+    for (int i = cardNumber.size() -2; i >= 0; i-= 2)
+    {
+        sum += getDigit((cardNumber[i] - '0') * 2);
+    }
+
+    return sum;
+    
 }
