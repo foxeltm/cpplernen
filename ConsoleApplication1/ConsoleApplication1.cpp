c2018b7d189120464ea2c523b4a66913713d1d9b
foxeltm <fuchs.raffael@hotmail.com>
2025-12-10 20:04:51 +0100

    dynamic memory

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -2,27 +2,33 @@
 #include <ctime>
 #include <string>
 
-template<typename L,int N>
-class Array
-{
-private:
-    L m_Array[N];
-public:
-    int Getsize() const { return N; }
-};
 template<typename T>
-void Log(T value)
+void Log (T value)
 {
     std::cout << value << std::endl;
 }
 
 int main()
 {	    
-    Array<int ,5> array;
-    Log(array.Getsize());
+    char* pGrades = NULL;
+    int size;
 
-    Log("Marvin is the GOAT");
+    Log("How many grades to enter in? ");
+    std::cin >> size;
 
+    pGrades = new char[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        std::cout << "Enter gerade #" << i + 1 << ": ";
+        std::cin >> pGrades[i];
+    }
+    for (int i = 0; i < size; i++)
+    {
+        std::cout << pGrades[i] << " ";
+    }
+
+    delete[] pGrades;
 
     std::cin.get();
 }
