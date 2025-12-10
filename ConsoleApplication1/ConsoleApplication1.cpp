8562543ed4aab781b1703c94f65caf2769969d85
foxeltm <fuchs.raffael@hotmail.com>
2025-12-10 15:21:10 +0100

    Templates"

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -2,23 +2,26 @@
 #include <ctime>
 #include <string>
 
+template<typename L,int N>
+class Array
+{
+private:
+    L m_Array[N];
+public:
+    int Getsize() const { return N; }
+};
+template<typename T>
+void Log(T value)
+{
+    std::cout << value << std::endl;
+}
+
 int main()
 {	    
-    int *pointer = nullptr;
-    int x = 123;
+    Array<int ,5> array;
+    Log(array.Getsize());
 
-    pointer = &x;
-
-
-    if (pointer == nullptr)
-    {
-        std::cout << "address was not assigned\n";
-    }
-    else
-    {
-        std::cout << "address was assigned\n";
-        std::cout << *pointer;
-    }
+    Log("Marvin is the GOAT");
 
 
     std::cin.get();
