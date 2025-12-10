bd4c17231f464a692316fc24cf761b6e76e36807
foxeltm <fuchs.raffael@hotmail.com>
2025-12-10 21:30:29 +0100

    STRUCTS

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -2,21 +2,40 @@
 #include <ctime>
 #include <string>
 
+struct student
+{
+    std::string name;
+    double gpa = 0;
+    bool enrolled;
+};
+
 template<typename T>
 void Log(T value)
 {
     std::cout << value << std::endl;
 }
 
-template<typename L, typename U>
-auto max(L x, U y)
-{
-    return (x > y) ? x : y;
-}
-
 int main()
 {	    
-    Log(max(1.6969, 2));
+    student student1;
+    student1.name = "Dominik";
+    student1.gpa = 3;
+    student1.enrolled = true;
+
+    student student2;
+    student2.name = "Theo";
+    student2.gpa = 500000;
+    student2.enrolled = false;
+
+    Log(student1.name);
+    Log(student1.gpa);
+    Log(student1.enrolled);
+
+    Log(student2.name);
+    Log(student2.gpa);
+    Log(student2.enrolled);
+
+
 
     std::cin.get();
 }
