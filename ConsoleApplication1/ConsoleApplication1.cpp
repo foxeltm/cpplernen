522b89f0c4db5bc45de783f3ac6e003037d35049
foxeltm <fuchs.raffael@hotmail.com>
2025-12-12 18:02:59 +0100

    structs as arguments

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -2,43 +2,49 @@
 #include <ctime>
 #include <string>
 
-struct student
-{
-    std::string name;
-    double gpa = 0;
-    bool enrolled;
-};
-
 template<typename T>
 void Log(T value)
 {
     std::cout << value << std::endl;
 }
 
-int main()
-{	    
-    student student1;
-    student1.name = "Dominik";
-    student1.gpa = 3;
-    student1.enrolled = true;
+struct Car {
+    std::string model;
+    int year;
+    std::string color;
+};
 
-    student student2;
-    student2.name = "Theo";
-    student2.gpa = 500000;
-    student2.enrolled = false;
+void printCar(Car& car);
+void paintCar(Car& car, std::string color);
 
-    Log(student1.name);
-    Log(student1.gpa);
-    Log(student1.enrolled);
+int main() {
 
-    Log(student2.name);
-    Log(student2.gpa);
-    Log(student2.enrolled);
+    Car car1;
+    Car car2;
 
+    car1.model = "Mustang";
+    car1.year = 2023;
+    car1.color = "red";
 
+    car2.model = "Corvette";
+    car2.year = 2024;
+    car2.color = "blue";
 
-    std::cin.get();
+    paintCar(car1, "silver");
+    paintCar(car2, "gold");
+
+    printCar(car1);
+    printCar(car2);
+
+    return 0;
 }
-
+void printCar(Car& car) {
+    std::cout << car.model << '\n';
+    std::cout << car.year << '\n';
+    std::cout << car.color << '\n';
+}
+void paintCar(Car& car, std::string color) {
+    car.color = color;
+}
 
 
