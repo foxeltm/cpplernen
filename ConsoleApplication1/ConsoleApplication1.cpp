610362770ef124f2e73645d1a467037b5eb8605e
foxeltm <fuchs.raffael@hotmail.com>
2026-01-01 21:27:43 +0100

    renderer

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -2,38 +2,9 @@
 #include <vector>
 #include <string>
 
-class Entity
-{
-public:
-    float X, Y;
-
-    Entity()
-    {
-        X = 0.0f;
-        Y = 0.0f;
-        std::cout << "Constructed Entity " << std::endl;
-    }
-
-    ~Entity()
-    {
-        std::cout << "Destroyed Entity! " << std::endl;
-    }
-
-    void Print()
-    {
-        std::cout << X << ", " << Y << std::endl;
-    }
-};
-
-void Function()
-{
-    Entity e;
-    e.Print();
-}
 
 int main() 
 {
-    Function();
 
 
     std::cin.get();
